e561053c66d9bbc6516ee1022b116034a7aafc0a
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-21 21:11:18 +0500

    nested

---
+++ conditionalStatements.py
@@ -0,0 +1,63 @@
+# n=20
+# if(n>=222):
+#     print(" number is greater :")
+# elif(n<=2):
+#     print(" number is lesser :")
+
+# num=100
+
+# if(num>33):
+#     print("pass :")
+# elif(num<33):
+#     print(" fail :")
+# else:
+#     print(" no result :")
+
+# num=int(input("enter the marks :"))
+
+# if(num>=90):
+#     grade="A"
+# elif(num<90 and num>=80):
+#      grade="B"
+# elif(80<num and num>=70):
+#      grade="C"
+# else:
+#      grade="D"
+# print("garde of the students :", grade)
+
+#  nesting
+
+# age = 22
+
+# if(age>18):
+#     if(age>60):
+#         print("will die.")
+
+#     print("can drive.")
+
+# else:
+
+#     print("both can't.")
+
+# num = int(input("Enter the num."))
+# if(num%2 == 0):
+#     print(" number is even.")
+# elif(num%2!= 0):
+#     print(" number is odd.")
+
+# n1=int(input("enter first num."))
+# n2=int(input("enter second num."))
+# n3=int(input("enter third num."))
+
+# if(n1>n2 and n1>n3):
+#     print(" n1 is greatest of three number.")
+# elif(n2>n1 and n2>n3):
+#     print(" n2 is greatest of three number.")
+# else:
+#     print(" n3 is greatest of three number.")
+
+n =int(input("enter  num."))
+if(n%7 == 0):
+    print("yes it is multiple.")
+else:
+    print("it is not.")
