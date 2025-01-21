cda1971f12b34c272a7a8e7f08b36c8fde28d197
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-21 17:16:20 +0500

    string basics

---
+++ strings.py
@@ -0,0 +1,45 @@
+# str=" this is python.\n first program."
+# print(str)
+
+# str1="python is programming "
+# str2="langauge"
+# print(str1+str2)
+
+# str1="python"
+# len1=len(str1)
+# print(str1)
+# print(len1)
+# str2="language"
+# str3=str1+"  "+str2
+# len2=len(str3)
+# print(len2)
+
+# str="python"
+# ch1=str[1]
+# ch2=str[4]
+# ch3=str[0]
+# print(ch1)
+# print(ch2)
+# print(ch3)
+# print(str[1:5])
+
+# str="pyhton"
+# print(str[-3:-1])
+
+# str="pyhton is the programming langauge."
+# print(str.endswith("ge."))
+# print(str.endswith("uge"))
+
+# str="i am studying pyhton."
+# print(str.capitalize())
+
+# str="i am learning python."
+# # print(str.replace("python","java"))
+# print(str.find("a"))
+# print(str.count("in"))
+
+# name=input("enter your name :")
+# print("len of your name is :",len(name))
+
+str="hi $ rate is in $ :"
+print(str.count("$"))
