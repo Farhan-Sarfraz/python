c4c0dac5f27917b70c3c03b57b6c7c0d22664e92
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-21 15:46:19 +0500

    intro

---
+++ variablesAndDatatype.py
@@ -0,0 +1,73 @@
+
+# print side of sqaure and its area
+# side=int(input(" side is : "))
+# print(side)
+# area=side*side
+# print("area of square is :",area)
+
+# write a program to input 2 floats and put averg
+
+first=float(input("first nmbr is :"))
+second=float(input("second nmbr is :"))
+avrg=(first+second)/2
+print(" Average is :",avrg)
+# a=int(input(" number a is :"))
+# b=int(input(" number b is :"))
+# print(" Sum :",a+b)
+
+# type casting
+# a=1
+# b=2.3
+# c=int(b)
+# sum=a+c
+# print("integer to float .",sum)
+#  type conversion
+# a=1
+# b=2.3
+# sum=a+b
+# print("integer to float .",sum)
+
+ #  arithmatics operators
+
+# a=2
+# b=1
+
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a % b)
+# print(a ** b)
+
+# print("hello")
+
+# print(" hello world")
+
+# print("python."," programming languagge.")
+
+# name="ali"
+# age=23
+# value=23.6
+# a=None
+# old=True
+
+# print(name)
+# print(age)
+# print(a)
+# print(old)
+# print(value)
+
+# print(type(name))
+# print(type(age))
+# print(type(value))
+# print(type(old))
+# print(type(a)) 
+
+# a=10
+# b=23
+# sum=a+b
+# print(sum)
+
+# input
+# name=input("enter your name :")
+# print(type(name),name)
