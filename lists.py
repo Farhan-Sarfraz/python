5b11add716e9184cbba62eeeeef5f29065ae326c
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-22 19:43:21 +0500

    lists

---
+++ lists.py
@@ -0,0 +1,62 @@
+# marks=[34,55,66,43,76,55,44,33]
+
+# print(marks)
+# print(type(marks))
+
+# print(len(marks))
+# print(marks[2])
+# print(marks[1])
+
+# lists=["ali",34,10.4,"kamran",22,44,55,66]
+# print(lists)
+# print(type(lists))
+# print(len(lists))
+# print(lists[4])
+
+# lists[3]="farhan"
+
+# print(lists)
+# print(type(lists))
+# print(len(lists))
+# print(lists[4])
+
+# marks=[23,43,65,86,76,89,90,75,63,23,91,12,76,78]
+# print(marks[3:6])
+# print(marks[3:])
+# print(marks[:6])
+# print(marks[-9:-3])
+# print(marks[:-1])
+# print(len(marks[3:6]))
+
+# list = [4,5,6,2,3]
+
+# print(value.append(0))
+# print(value)
+
+
+# print(list.sort())
+# print(list)
+
+# list=[4,2,7,1,0,9]
+# list.sort(reverse=True)
+# print(list)
+
+# fruits=["apple","mango","banana"]
+# # fruits.sort()
+# # print(fruits)
+
+# fruits.sort(reverse=True)
+# print(fruits)
+
+marks=[43,54,65,33]
+# marks.reverse()
+# print(marks)
+
+# marks.insert(2,0)
+# print(marks)
+
+# marks.remove(33)
+# print(marks)
+
+marks.pop(0)
+print(marks)
