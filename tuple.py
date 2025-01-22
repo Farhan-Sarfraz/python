37fc42b233a0883a2d50f53a241873e4961dfffd
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-22 21:36:07 +0500

    tuple

---
+++ tuple.py
@@ -0,0 +1,36 @@
+ 
+# tup=(1,2,5,4)
+
+# print(tup)
+# print(type(tup))
+# tup1=()
+# print(tup1)
+# print(type(tup1))
+
+# tup12=(1)
+# print(tup12)
+# print(type(tup12))
+
+# tup=(1,)
+# print(tup)
+# print(type(tup))
+
+# tup=(2,1,5,2,7,3)
+
+# print(tup)
+
+# print(tup[3:5])
+
+# tup=(5,6,2,0,1,9)
+
+# print(tup.index(1))
+
+# tup=(5,6,2,1,2,3,9)
+
+# print(tup.count(1))
+
+tup=('C','D','A','A','B','B','A')
+
+pro=tup.count('A')
+
+print(pro)
