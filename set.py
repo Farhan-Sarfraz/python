32c93872508cdebd4c8195a4bdcedc7367126ed5
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-26 20:30:50 +0500

    set

---
+++ set.py
@@ -0,0 +1,44 @@
+
+# set={1,2,3,4,"hello",1.3,4,2,9}
+# print(set)
+# print(len(set))
+# print(type(set))
+
+# collection = set() //emplty set
+# print(collection)
+# print(type(collection))
+
+# set= {1,2,34,4,}
+
+# print(set)
+
+# set.add(9)
+# set.remove(2)
+
+# print(set)
+
+# set= {1,2,34,4,}
+
+# print(len(set))
+
+# set.clear()
+# print(len(set))
+
+# set= {1,2,34,4,45,54,23,89}
+
+# print(set.pop())
+# print(set.pop())
+# print(set.pop())
+# print(set.pop())
+
+
+set1={1,2,3,4}
+set2={3,4,5,6}
+
+print(set1.union(set2))
+
+
+set1={1,2,3,4}
+set2={3,4,5,6}
+
+print(set1.intersection(set2))
