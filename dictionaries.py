bb4f58191891f8698fe708e9346746eb17fc4f66
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-01-26 14:00:42 +0500

    about dict

---
+++ dictionaries.py
@@ -0,0 +1,89 @@
+
+# dic={
+
+#     "key" : "value",
+#     "Name" : "ali",
+#     "rollnum" : 21,
+#     "marks" : 99.3,
+#     "subj" : ["ss","eng","urdu"],
+#     77 : 88.6,
+# }
+
+# print(dic)
+# print(type(dic))
+
+# dic={
+
+#     "key" : "value",
+#     "Name" : "ali",
+#     "rollnum" : 21,
+#     "marks" : 99.3,
+#     "subj" : ["ss","eng","urdu"],
+#     77 : 88.6,
+# }
+
+# print(dic["Name"])
+
+# print(dic["key"])
+
+# student = {
+#     " name " : " ali ",
+#     " subjects "  : {
+#         "chem"  :  842,
+#         "bio"  : 785,
+#         "math"  : 237,
+#     }
+# }
+# print(student)
+# print(student[" subjects "])
+# print(student[" subjects "]["bio"])
+
+# student = {
+#     " name " : " ali ",
+#     " subjects "  : {
+#         "chem"  :  842,
+#         "bio"  : 785,
+#         "math"  : 237,
+#     }
+# }
+# print(student.keys())
+# print(len(student))
+
+# print(list(student))
+
+# student = {
+#     " name " : " ali ",
+#     " subjects "  : {
+#         "chem"  :  842,
+#         "bio"  : 785,
+#         "math"  : 237,
+#     }
+# }
+# print(student.items())
+# print(len(student))
+
+# print(list(student))
+
+# student = {
+#     " name " : " ali ",
+#     " subjects "  : {
+#         "chem"  :  842,
+#         "bio"  : 785,
+#         "math"  : 237,
+#     }
+# }
+# print(student.get(" name "))
+
+
+student = {
+    " name " : " ali ",
+    " subjects "  : {
+        "chem"  :  842,
+        "bio"  : 785,
+        "math"  : 237,
+    }
+}
+print(student.update({"city":"muy"}))
+print(student)
+
+
