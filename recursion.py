ea46ee9ec555c52f06a1289d71e298b38f85dd15
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-02-02 20:26:33 +0500

    recursive functions

---
+++ recursion.py
@@ -0,0 +1,46 @@
+
+# def num(n):
+#     if(n==0):
+#         return 
+#     print(n)
+#     num(n-1)
+# num(5)
+
+# def num(n):
+#     # if(n==0):
+#     #     return 
+#     print(n)
+#     num(n-1)
+# num(5)
+
+# def fact(n):
+#     # base case
+#     if(n==1 or n==0 ):
+#         return 1
+#     # recursive case
+#     return fact(n-1) * n 
+
+# print(fact(3))
+
+# def fact(n):
+#     if( n == 1 or n == 0):
+#         return 1
+#     return fact(n-1) * n
+# print(fact(7))
+
+# def cal(n):
+#     if( n == 0 ):
+#         return 0
+#     return cal(n-1) + n
+# sum=(cal(5))
+# print(sum)
+
+# def print_list(list,idx=0):
+#     if(idx==len(list)):
+#         return
+#     print(list[idx])
+#     print(list,idx+1)
+# number = ["aus","pak","cina count"]
+# print_list(number)
+    
+ 
