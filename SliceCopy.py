8eee8b941102d44b953886321e3195859a699c21
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-04-19 18:08:30 +0500

    SliceCopyRef

---
+++ SliceCopy.py
@@ -0,0 +1,55 @@
+# l1 = [1,2,3]
+# l2 = l1[:]
+
+# print(l1)
+# print(l2)
+
+# l1[1] = 55
+
+# print(l1)
+# print(l2)
+
+# list1 = [1,2,3,4]
+# list2 = list1[0:2]
+
+# print(list1)
+# print(list2)
+
+# list1[1] = 33
+
+# print(list1)
+# print(list2)
+
+# list2 = [3,4,5,6]
+
+# print(list1)
+# print(list2)
+
+
+# m = [1,2,3]
+# n = m
+
+# print(m)
+# print(n)
+
+# if(m == n):
+#     print(True)
+# else:
+#     print("False")
+
+m = [1,2,3]
+n = m 
+
+print(m)
+print(n)
+
+n = [1,2,3]
+
+if(m == n):
+    print("true")
+else:
+    print("false")
+if(m is n):
+    print("true")
+else:
+    print("false")
