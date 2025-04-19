561c264a43a8630a90d56d002f582057cde3a759
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-04-19 17:42:02 +0500

    mutable&immutable

---
+++ mutable.py
@@ -0,0 +1,69 @@
+
+# a = [1,2,3]
+# b = a
+# # print(a)
+# # print(b)
+# a[0] = [99]
+# # print(a)
+# # print(b)
+# b=[1,2,3]
+# print(a)
+# print(b)
+
+# str1 = "abc"
+
+# print(str1)
+# str1[1] = 4
+
+# list = [12,33,44]
+
+# print(list)
+
+# list[2] = 9
+
+# print(list)
+
+# tuple = (1,2,3,4)
+# print(tuple)
+
+# tuple[1] = 33
+
+# print(tuple)
+
+# dict = {
+# "num" : 23,
+# "str12" : "thanks",
+
+# }
+# print(dict)
+
+# dict[1] =  "new"
+# print(dict)
+
+# set = {2,3,4,5,5}
+
+# print(set)
+
+# print(len(set))
+
+# print(type(set))
+
+# set.add(6)
+
+# print(set)
+
+# set.remove(2)
+
+# print(set)
+
+
+x = 10
+y = x
+print(x)
+print(y)
+
+x = 11
+
+print(x)
+print(y)
+
