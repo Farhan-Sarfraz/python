4d5987556696b673dc51bc579dde34fdde7ac440
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-04-21 22:36:26 +0500

    Number_basics

---
+++ Numbers.py
@@ -0,0 +1,87 @@
+# import math
+
+# num = math.floor(3.4)
+# num1 = math.floor(3.9)
+# num2 = math.floor(13.1)
+# num3 = math.floor(-3.9)
+
+# print(num)
+# print(num1)
+# print(num2)
+# print(num3)
+
+# 
+# import math
+
+# v1 = math.trunc(3.3)
+# v2 = math.trunc(3.9)
+# v3 = math.trunc(-3.3)
+# v4 = math.trunc(-9.7)
+
+# print(v1)
+# print(v2)
+# print(v3)
+# print(v4)
+
+# n = 2 + 4j
+# n1 = n * 3
+#              complex number are also handled  
+# print(n1)
+
+
+
+
+# num = 3 * 4 + 3
+
+# print(num)
+
+# num1 = 4 / 2 * 10 - 11 + 25
+
+# print(num1)
+
+
+# def fact_num(n):
+#     fact = 1
+#     for num in range(1,n+1):
+#         fact *= num
+#     return fact
+
+# factorial = fact_num(5)
+
+# print(factorial)
+
+# def fact_num(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * fact_num(n - 1)
+# factorial = fact_num(5)
+# print("factorial of n is : >>> ", factorial)
+
+
+# num = 20
+
+# hex_val = hex(num)
+# oct_val = oct(num)
+# bin_val = bin(num)
+
+# print(hex_val)
+# print(oct_val)
+# print(bin_val)
+
+# binary = int('10000' , 2)
+# octal  = int('20' , 8)
+# hexal  = int('20' , 16)
+
+# print(binary)
+# print(hexal)
+# print(octal)
+
+# import random
+
+# num = random.random()
+# print(num)
+
+# n = 0.1 + 0.1 + 0.1 - 0.3
+#                           wrong 
+# print(n)
+
