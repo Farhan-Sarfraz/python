0305b28a3e05eaf5a90788f54abc4eb943f16355
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-04-26 20:41:55 +0500

    10 problem_solved

---
+++ Conditional_problems.py
@@ -0,0 +1,119 @@
+
+# age = int(input("Enter the age : "))
+
+# if age < 13:
+#     print("child")
+# elif age < 19:
+#     print("teenager")
+# elif age < 60:
+#     print("adault")
+# elif age > 60:
+#     print("senior")
+# else:
+#     print("Please enter right input : ")
+
+# age = int(input("enter the age : "))
+
+# day = "wed"
+# price = 12 if age >= 18 else 8
+
+# if day == "wed":
+#     price -= 2
+
+# print("ticket price is :",price)
+
+# age = int(input("enter the age :"))
+
+# day = "wed" 
+# price = 0
+
+# if age > 19:
+#     price = 12
+# else:
+#     price = 8
+
+# if day == "wed":
+#     price -= 2
+
+# print(price)
+
+
+# marks = int(input("enter the marks : " ))
+
+# if marks >= 101:
+#     print("PLease verify your number again : ")
+#     exit()
+
+# if marks > 90:
+#     grade ='A'
+# elif marks > 80:
+#     grade ='B'
+# elif marks > 70:
+#     grade ='C'
+# elif marks > 60:
+#     grade ='D'
+# else:
+#     grade = 'F'
+
+# print("Grade is : ", grade)
+
+# fruit = "banana"
+# color ="green"
+# if fruit == "banana":
+#     if color == "green":
+
+#         print("ripe")
+#     elif color == "brown":
+#         print("overriped")
+#     elif color == "yellow":
+#         print("unriped")
+
+# weather = "sunny"
+
+# if weather == "sunny":
+#     activity = "go for a walk"
+# elif weather == "rainy":
+#     activity = "read a book"
+# elif weather == "snowy":
+#     activity = "build a snowman"
+
+# print(activity)
+
+# dist = int(input("enter the distance you want : "))
+
+# if dist < 3:
+#     transportation = "walk"
+# elif dist < 15:
+#     transportation = "bike"
+# elif dist > 15:
+#     transportation = "car"
+
+# print(transportation)
+
+# order_size = "medium"
+# extra_shot = True
+
+# if extra_shot:
+#     coffee = order_size + " coffee with extra_shot "
+# else:
+#     coffee = order_size + "coffee"
+
+# print("order : ",coffee)
+
+# chars = int(input("enter the value : "))
+
+# if chars < 6:
+#     password = "weak"
+# elif chars < 10:
+#     password = "medium"
+# else:
+#     password = "strong"
+# print(password)
+
+# year = int(input("enter the year : "))
+
+# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+#     print("its leap year. ")
+# else:
+#     print("it's not a leap year.")
+
