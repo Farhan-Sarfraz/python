6c169021684be48f02928db1b40ab991ef24d66d
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-07-12 12:00:18 +0500

    file added

---
+++ random_module.py
@@ -0,0 +1,32 @@
+# import random
+
+# random_integer = random.randint(1, 10)
+
+# print(random_integer)
+
+# import random
+
+# random_float = random.random() * 5
+
+# print(random_float)
+
+# import random
+
+# random_int = random.randint(0,1)
+# if random_int == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+# import random
+
+# # roll_dice = random.randint(1,6)
+# # print("you rolled the dice",roll_dice)
+# import string
+# length = 10
+# all_chr = string.ascii_letters + string.digits
+# #  _ is a placeholder and the .join used of the joining the random numbers 
+# password = ''.join(random.choice(all_chr) for _ in range(length))
+
+# print("password is ", password)
