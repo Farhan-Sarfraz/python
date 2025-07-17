25ca4c9ce14e2e290f6cebd3aacaac9be26110e1
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-07-17 10:29:00 +0500

    file added

--- dictionaries.py
+++ dictionaries.py
@@ -113,16 +113,40 @@
 
 # print(marks)
 
-student = {
-    "name" : "ali",
-    "roll" : 9,
-    "subj" : {
-        "math" : 77,
-        "eng" : 73,
-    }
+# student = {
+#     "name" : "ali",
+#     "roll" : 9,
+#     "subj" : {
+#         "math" : 77,
+#         "eng" : 73,
+#     }
     
-}
+# }
 
 # print(student["subj"]["eng"])
 # print(type(student))
-print(student.values())
+# print(student.values())
+
+# dictionaries = {
+#     "bug" :  "error in program that prevents the program to run. ",
+#     "function" : "piece of data that call over again and again. ",
+#     "loop" : "action that have been performed repeadetly. ",
+#     "new" : "something to happened soon. "
+# }
+
+# print(dictionaries.keys())
+# print(dictionaries.values())
+# print(dictionaries.items())
+# print(dictionaries["loop"])
+# print(dictionaries)
+# dictionaries["new"] = "something is changed."
+# print(dictionaries)
+# dictionaries = {}
+# print(dictionaries)
+
+# for key in dictionaries:
+#     print(key)
+#     print(dictionaries[key])
+
+
+
