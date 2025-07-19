bd6bfe7ba37b58f80a3312962eb5562231b0e89c
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-07-19 11:42:13 +0500

    file added

---
+++ while_loops.py
@@ -0,0 +1,45 @@
+
+#  printing the sum of the number until the user enter the 0
+ 
+# total = 0
+# num = int(input("Enter a number (0 to stop): "))
+# while num != 0:
+#     total += num
+#     num = int(input("Enter another number (0 to stop): "))
+# print("Total sum is:", total)
+
+
+#  enter the valid numbering
+
+user_input = input("Enter a number between 1 and 10: ")
+while not 1<= int(user_input) <=10:
+
+    print("invalid nmber")
+    user_input = input("enter the number between 1 and 10.")
+user_input = int(user_input)
+print("user number is ", user_input)
+
+
+
+#  counting downward
+
+# count = 5
+# while count > 0:
+#     print("countdown", count)
+#     count -= 1
+# print("blast off ")
+
+# i = 1
+
+# while i <= 5:
+#     print(i)
+
+#     i += 1
+# i = 2
+# while i < 10:
+#         print(i)
+#         i += 2
+
+
+#  printing the number until user enter the zero 
+
