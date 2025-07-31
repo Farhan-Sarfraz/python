1151405a08e21f7c34ff1259241fcee22a990771
Farhan Sarfaraz <engrfarhansarfraz@gmail.com>
2025-07-31 11:44:35 +0500

    file added

--- recursion.py
+++ recursion.py
@@ -68,13 +68,47 @@
 # sum=calSum(5)
 # print(sum)
 
-def print_list(list,idx=0):
-    if(idx == len(list)):
-        return 
+# def print_list(list,idx=0):
+#     if(idx == len(list)):
+#         return 
     
-    print(list[idx])
-    print_list(list,idx+1)
+#     print(list[idx])
+#     print_list(list,idx+1)
 
-vl = [2,3,5,6,7,9,0]
+# vl = [2,3,5,6,7,9,0]
 
-print_list(vl)
+# print_list(vl)
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * factorial(n-1)
+
+# print(factorial(5))
+    
+# def sum(n):
+#     if n == 0:
+#         return 0
+#     return sum(n-1) + n
+# print("sum is ",sum(5))
+
+
+# def fabnocci(n):
+#     if n <= 1:
+#         return n
+#     return fabnocci(n-1) + fabnocci(n - 2)
+# for i in range(10):
+#     print(fabnocci(i), end=" ")
+    
+# def reverse_string(str):
+#     if str == "":
+#         return str
+#     return reverse_string(str[1:]) + str[0]
+# reverse_string = reverse_string("pakistan")
+# print(reverse_string)
+
+def power_of_num(base, exp):
+    if exp == 0:
+        return 1
+    return base * power_of_num(base, exp - 1)
+print(power_of_num(2, 5))
